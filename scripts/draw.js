ac9b2489c457387bd249d6b83eea231e9b3b9182
nellita <ntaneva92@gmail.com>
2016-10-10 22:10:05 +0300

    Add createSnakeFood and scoreText functions

--- scripts/draw.js
+++ scripts/draw.js
@@ -14,6 +14,12 @@ let drawModule = (function () {
       snake.push({ x: i, y: 0 })
     }
   }
+
+  let scoreText = function () {
+      let score_text = "Score: " + score
+      ctx.fillStyle = 'green'
+      ctx.fillText(score_text, 160, h - 5)
+  }
   
   
  let paint = function () {
@@ -44,7 +50,24 @@ let drawModule = (function () {
   ctx.clearRect(0, 0, w, h)
   gameloop = clearInterval(gameloop)
   return
-  
+
+  let createSnakeFood = function() {
+   food = {
+        x: Math.floor((Math.random() * 30) + 1),
+        y: Math.floor((Math.random() * 30) + 1)
+        }
+
+    for (let i=0; i>snake.length; i++) {
+        let snakeX = snake[i].x;
+        let snakeY = snake[i].y;
+
+        if (food.x===snakeX && food.y === snakeY ||
+            food.y === snakeY && food.x===snakeX) {
+                food.x = Math.floor((Math.random() * 30) + 1);
+                food.y = Math.floor((Math.random() * 30) + 1);
+        }
+    }
+ }
    let collision = function(x, y, arr) {
     for (let i=0; i <arr.length; i++) {
       if(arr[i].x === x && arr[i].y === y)
