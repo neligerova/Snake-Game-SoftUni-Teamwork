0d7ef0b2aba0bc3b5b61e841533c59ec4d4d3efb
vikobg <viko@vikobg.com>
2016-10-12 22:43:14 +0300

    app js update

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,44 @@
-
 (function (window, document, drawModule, undefined) {
 
-    let btn = document.getElementById('btn')
-    btn.addEventListener("click", function(){ drawModule.init()})
+var btn = document.getElementById('btn');
+btn.addEventListener("click", function(){ drawModule.init();});
 
-    window.addEventListener('keydown', kbdHandler)
+	document.onkeydown = function(event) {
 
-    function kbdHandler(event) {
+        keyCode = window.event.keyCode; 
+        keyCode = event.keyCode;
 
+        switch(keyCode) {
+        
+        case 37: 
+          if (direction != 'right') {
+            direction = 'left';
+          }
+          console.log('left'); 
+          break;
 
+        case 39:
+          if (direction != 'left') {
+          direction = 'right';
+          console.log('right');
+          }
+          break;
 
+        case 38:
+          if (direction != 'down') {
+          direction = 'up';
+          console.log('up');
+          }
+          break;
 
-        switch(event.code) {
-            case "ArrowLeft":
-                if (direction != 'right') {
-                    direction = 'left'
-                }
-
-                break
-
-            case "ArrowRight":
-                if (direction != 'left') {
-                    direction = 'right'
-
-                }
-                break
-
-            case "ArrowUp":
-                if (direction != 'down') {
-                    direction = 'up'
-
-                }
-                break
-
-            case "ArrowDown":
-                if (direction != 'up') {
-                    direction = 'down'
-
-                }
-                break
-        }
-
-    }
+        case 40:
+          if (direction != 'up') {
+          direction = 'down';
+          console.log('down');
+          }
+          break;
+          }
+      }
 
 
-}) (window, document, drawModule)
+})(window, document, drawModule);
