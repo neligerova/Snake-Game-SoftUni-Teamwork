331433d9bb27f271410aa2f92aec797aab7411a1
vikobg <viko@vikobg.com>
2016-10-12 21:52:09 +0300

    draw changes

--- scripts/draw.js
+++ scripts/draw.js
@@ -7,14 +7,6 @@ let drawModule = (function () {
     ctx.strokeRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize)
   }
 
-  let snakeDraw = function () {
-    let length = 4
-    let snake = []
-    for (let i = length - 1; i >= 0; i--) {
-      snake.push({ x: i, y: 0 })
-    }
-  }
-
   let hamburger = function () {
   let hamburger = new Image()
     hamburger.src  = './img/hamburger.png'
@@ -27,35 +19,64 @@ let drawModule = (function () {
       ctx.fillText(score_text, 160, h - 5)
   }
   
+ let snakeDraw = function () {
+    let length = 4
+    let snake = []
+    for (let i = length - 1; i >= 0; i--) {
+      snake.push({ x: i, y: 0 })
+    }
+  }  
   
  let paint = function () {
-    ctx.fillStyle = 'lightgrey'
-    ctx.fillRect(0, 0, w, h)
-    ctx.strokeStyle = 'black'
-    ctx.strokeRect(0, 0, w, h)
-  }
+   ctx.fillStyle = 'lightgrey';
+      ctx.fillRect(0, 0, w, h);
+      ctx.strokeStyle = 'black';
+      ctx.strokeRect(0, 0, w, h);
 
-  btn.setAttribute('disabled', true)
+      btn.setAttribute('disabled', true);
 
+      var snakeX = snake[0].x;
+      var snakeY = snake[0].y;
 
+      if (direction == 'right') { 
+        snakeX++; }
+      else if (direction == 'left') { 
+        snakeX--; }
+      else if (direction == 'up') { 
+        snakeY--; 
+      } else if(direction == 'down') { 
+        snakeY++; }
 
-  let snakeX = snake[0].x
-  let snakeY = snake[0].y
+      if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || checkCollision(snakeX, snakeY, snake)) {
+          //restart game
+          btn.removeAttribute('disabled', true);
 
-  if(direction == 'right') {
-    snakeX++
-  } else if (direction == 'left') {
-    snakeX--
-  } else if (direction == 'up') {
-    snakeY--
-  } else if (direction == 'down') {
-    snakeY++ }
+          ctx.clearRect(0,0,w,h);
+          gameloop = clearInterval(gameloop);
+          return;          
+        }
+        
+        if(snakeX == food.x && snakeY == food.y) {
+          var tail = {x: snakeX, y: snakeY}; //Create a new head instead of moving the tail
+          score ++;
+          
+          createFood(); //Create new food
+        } else {
+          var tail = snake.pop(); //pops out the last cell
+          tail.x = snakeX; 
+          tail.y = snakeY;
+        }
+        //The snake can now eat the food.
+        snake.unshift(tail); //puts back the tail as the first cell
+
+        for(var i = 0; i < snake.length; i++) {
+          bodySnake(snake[i].x, snake[i].y);
+        } 
+        
+        pizza(food.x, food.y); 
+        scoreText();
   
-  if (snakeX == -1 || snakeX == w / snakeSize || snakeY == -1 || snakeY == h / snakeSize || collision(snakeX, snakeY, snake))
-      btn.removeAttribute('disabled', true)
-  ctx.clearRect(0, 0, w, h)
-  gameloop = clearInterval(gameloop)
-  return
+ }
 
   let createSnakeFood = function() {
    food = {
@@ -83,7 +104,17 @@ let drawModule = (function () {
 
   }
 
+ let init = function(){
+      direction = 'down';
+      snakeDraw();
+      createSnakeFood();
+      gameloop = setInterval(paint, 80);
+  }
 
+
+    return {
+      init : init
+    };
 
 
 
