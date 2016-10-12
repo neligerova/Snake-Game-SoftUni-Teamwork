7d2d879026a88afa71063fb73ef70bd94d98e2f8
Svetlana Kostadinova <sikostadinova@gmail.com>
2016-10-12 06:26:58 +0300

    kbdHandller

--- scripts/app.js
+++ scripts/app.js
@@ -18,27 +18,27 @@
                 if (direction != 'right') {
                     direction = 'left'
                 }
-                console.log('left')
+
                 break
 
             case "ArrowRight":
                 if (direction != 'left') {
                     direction = 'right'
-                    console.log('right')
+
                 }
                 break
 
             case "ArrowUp":
                 if (direction != 'down') {
                     direction = 'up'
-                    console.log('up')
+
                 }
                 break
 
             case "ArrowDown":
                 if (direction != 'up') {
                     direction = 'down'
-                    console.log('down')
+
                 }
                 break
         }
