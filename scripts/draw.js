883e2af8a1c4cff35412c3ef2d0d88311630ae1d
vikobg <viko@vikobg.com>
2016-10-13 08:02:37 +0300

    bug fixed

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,34 +1,34 @@
-let drawModule = (function () { 
+var drawModule = (function () { 
 
-  let bodySnake = function(x, y) {
+  var bodySnake = function(x, y) {
         ctx.fillStyle = 'green';
         ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
         ctx.strokeStyle = 'darkgreen';
         ctx.strokeRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
   }
 
-  let pizza = function(x, y) {
+  var pizza = function(x, y) {
         ctx.fillStyle = 'yellow';
         ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
         ctx.fillStyle = 'red';
         ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
   }
 
-  let scoreText = function() {
-    let score_text = "Score: " + score;
+  var scoreText = function() {
+    var score_text = "Score: " + score;
     ctx.fillStyle = 'blue';
     ctx.fillText(score_text, 145, h-5);
   }
 
-  let drawSnake = function() {
-      let length = 4;
+  var drawSnake = function() {
+      var length = 4;
       snake = [];
-      for (let i = length-1; i>=0; i--) {
+      for (var i = length-1; i>=0; i--) {
           snake.push({x:i, y:0});
       }  
   }
     
-  let paint = function(){
+  var paint = function(){
       ctx.fillStyle = 'lightgrey';
       ctx.fillRect(0, 0, w, h);
       ctx.strokeStyle = 'black';
@@ -36,8 +36,8 @@ let drawModule = (function () {
 
       btn.setAttribute('disabled', true);
 
-      let snakeX = snake[0].x;
-      let snakeY = snake[0].y;
+      var snakeX = snake[0].x;
+      var snakeY = snake[0].y;
 
       if (direction == 'right') { 
         snakeX++; }
@@ -51,8 +51,6 @@ let drawModule = (function () {
       if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || checkCollision(snakeX, snakeY, snake)) {
           //restart game
           btn.removeAttribute('disabled', true);
-          
-          score = 0;
 
           ctx.clearRect(0,0,w,h);
           gameloop = clearInterval(gameloop);
@@ -60,19 +58,19 @@ let drawModule = (function () {
         }
         
         if(snakeX == food.x && snakeY == food.y) {
-          let tail = {x: snakeX, y: snakeY}; //Create a new head instead of moving the tail
+          var tail = {x: snakeX, y: snakeY}; //Create a new head instead of moving the tail
           score ++;
           
           createFood(); //Create new food
         } else {
-          let tail = snake.pop(); //pops out the last cell
+          var tail = snake.pop(); //pops out the last cell
           tail.x = snakeX; 
           tail.y = snakeY;
         }
         //The snake can now eat the food.
         snake.unshift(tail); //puts back the tail as the first cell
 
-        for(let i = 0; i < snake.length; i++) {
+        for(var i = 0; i < snake.length; i++) {
           bodySnake(snake[i].x, snake[i].y);
         } 
         
@@ -80,15 +78,15 @@ let drawModule = (function () {
         scoreText();
   }
 
-  let createFood = function() {
+  var createFood = function() {
       food = {
         x: Math.floor((Math.random() * 30) + 1),
         y: Math.floor((Math.random() * 30) + 1)
       }
 
       for (var i=0; i>snake.length; i++) {
-        let snakeX = snake[i].x;
-        let snakeY = snake[i].y;
+        var snakeX = snake[i].x;
+        var snakeY = snake[i].y;
       
         if (food.x===snakeX && food.y === snakeY || food.y === snakeY && food.x===snakeX) {
           food.x = Math.floor((Math.random() * 30) + 1);
@@ -97,15 +95,15 @@ let drawModule = (function () {
       }
   }
 
-  let checkCollision = function(x, y, array) {
-      for(let i = 0; i < array.length; i++) {
+  var checkCollision = function(x, y, array) {
+      for(var i = 0; i < array.length; i++) {
         if(array[i].x === x && array[i].y === y)
         return true;
       } 
       return false;
   }
 
-  let init = function(){
+  var init = function(){
       direction = 'down';
       drawSnake();
       createFood();
