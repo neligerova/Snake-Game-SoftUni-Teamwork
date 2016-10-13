ee6ba28111fa1687d164a735be1200a08d9028d8
nellita <ntaneva92@gmail.com>
2016-10-13 08:20:11 +0300

    Update draw file

--- scripts/draw.js
+++ scripts/draw.js
@@ -51,7 +51,7 @@ var drawModule = (function () {
       if (snakeX == -1 || snakeX == w/snakeSize || snakeY == -1 || snakeY == h/snakeSize || checkCollision(snakeX, snakeY, snake)) {
           //restart game
           btn.removeAttribute('disabled', true);
-
+          score=0
           ctx.clearRect(0,0,w,h);
           gameloop = clearInterval(gameloop);
           return;          
